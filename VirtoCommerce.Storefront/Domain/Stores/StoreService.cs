alexbobrob/VirtoCommerce-Storefront-ASP.NET

commit 79a63c89bc038acdb7784e37ebf55dc24db79516

Get Store and PaymentMethods in StoreService (need to the Platform v.3) (#264)
Get Store and PaymentMethods in StoreService (need to the Platform v.3)

VirtoCommerce.Storefront/Domain/Stores/StoreService.cs
@@ -1,15 +1,21 @@
-using Microsoft.Extensions.Caching.Memory;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Memory;
+using VirtoCommerce.Storefront.AutoRestClients.PaymentModuleApi;
+using VirtoCommerce.Storefront.AutoRestClients.PaymentModuleApi.Models;
+using VirtoCommerce.Storefront.AutoRestClients.PlatformModuleApi.Models;
 using VirtoCommerce.Storefront.AutoRestClients.StoreModuleApi;
+using VirtoCommerce.Storefront.AutoRestClients.TaxModuleApi;
+using VirtoCommerce.Storefront.AutoRestClients.TaxModuleApi.Models;
+using VirtoCommerce.Storefront.Common;
 using VirtoCommerce.Storefront.Infrastructure;
-using VirtoCommerce.Storefront.Model.Common.Caching;
-using VirtoCommerce.Storefront.Model.Stores;
-using VirtoCommerce.Storefront.Extensions;
-using System.Collections.Generic;
-using VirtoCommerce.Storefront.Caching;
 using VirtoCommerce.Storefront.Model.Caching;
 using VirtoCommerce.Storefront.Model.Common;
+using VirtoCommerce.Storefront.Model.Common.Caching;
+using VirtoCommerce.Storefront.Model.Stores;
+using StorePaymentMethod = VirtoCommerce.Storefront.AutoRestClients.StoreModuleApi.Models.PaymentMethod;
+using TaxProvider = VirtoCommerce.Storefront.AutoRestClients.StoreModuleApi.Models.TaxProvider;
 
 namespace VirtoCommerce.Storefront.Domain
 {
@@ -21,11 +27,16 @@ namespace VirtoCommerce.Storefront.Domain
         private readonly IStoreModule _storeApi;
         private readonly IStorefrontMemoryCache _memoryCache;
         private readonly IApiChangesWatcher _apiChangesWatcher;
-        public StoreService(IStoreModule storeApi, IStorefrontMemoryCache memoryCache, IApiChangesWatcher apiChangesWatcher)
+        private readonly IPaymentModule _paymentModule;
+        private readonly ITaxModule _taxModule;
+
+        public StoreService(IStoreModule storeApi, IStorefrontMemoryCache memoryCache, IApiChangesWatcher apiChangesWatcher, IPaymentModule paymentModule, ITaxModule taxModule)
         {
             _storeApi = storeApi;
             _memoryCache = memoryCache;
             _apiChangesWatcher = apiChangesWatcher;
+            _paymentModule = paymentModule;
+            _taxModule = taxModule;
         }
         public async Task<Model.Stores.Store[]> GetAllStoresAsync()
         {
@@ -36,7 +47,61 @@ namespace VirtoCommerce.Storefront.Domain
                 cacheEntry.AddExpirationToken(_apiChangesWatcher.CreateChangeToken());
 
                 return (await _storeApi.GetStoresAsync()).Select(x => x.ToStore()).ToArray();
-            }, cacheNullValue : false);
+            }, cacheNullValue: false);
+        }
+
+        public async Task<Store> GetStoreByIdAsync(string id, Currency currency)
+        {
+            var storeDto = await _storeApi.GetStoreByIdAsync(id);
+            var result = storeDto.ToStore();
+
+            if (!storeDto.PaymentMethods.IsNullOrEmpty() && currency != null)
+            {
+                result.PaymentMethods = storeDto.PaymentMethods
+                    .Where(pm => pm.IsActive != null && pm.IsActive.Value)
+                    .Select(pm => pm.ToStorePaymentMethod(currency)).ToArray();
+            }
+            else if (storeDto.PaymentMethods.IsNullOrEmpty() && currency != null)
+            {
+                var paymentSearchCriteria = new PaymentMethodsSearchCriteria { StoreId = id };
+                var paymentsSearchResult = await _paymentModule.SearchPaymentMethodsAsync(paymentSearchCriteria);
+
+                result.PaymentMethods = paymentsSearchResult.Results
+                    .Where(pm => pm.IsActive != null && pm.IsActive.Value)
+                    .Select(pm =>
+                    {
+                        var paymentMethod = pm.JsonConvert<StorePaymentMethod>().ToStorePaymentMethod(currency);
+                        paymentMethod.Name = pm.TypeName;
+                        return paymentMethod;
+                    }).ToArray();
+            }
+
+            if (storeDto.TaxProviders.IsNullOrEmpty())
+            {
+                var taxSearchCriteria = new TaxProviderSearchCriteria { StoreId = id };
+                var taxProviderSearchResult = await _taxModule.SearchTaxProvidersAsync(taxSearchCriteria);
+                result.FixedTaxRate = GetFixedTaxRate(taxProviderSearchResult.Results.Select(xp => xp.JsonConvert<TaxProvider>()).ToArray());
+            }
+            else
+            {
+                result.FixedTaxRate = GetFixedTaxRate(storeDto.TaxProviders);
+            }
+
+            return result;
+        }
+
+        private decimal GetFixedTaxRate(IList<TaxProvider> taxProviders)
+        {
+            var result = 0m;
+            var fixedTaxProvider = taxProviders.FirstOrDefault(x => x.IsActive.GetValueOrDefault(false) && x.Code == "FixedRate");
+            if (fixedTaxProvider != null && !fixedTaxProvider.Settings.IsNullOrEmpty())
+            {
+                result = fixedTaxProvider.Settings
+                    .Select(x => x.JsonConvert<Setting>().ToSettingEntry())
+                    .GetSettingValue("VirtoCommerce.Core.FixedTaxRateProvider.Rate", 0.00m);
+            }
+
+            return result;
         }
     }
 }

VirtoCommerce.Storefront.Model/Stores/IStoreService.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 using VirtoCommerce.Storefront.Model.Common;
 
@@ -9,5 +6,6 @@ namespace VirtoCommerce.Storefront.Model.Stores
     public interface IStoreService
     {
         Task<Store[]> GetAllStoresAsync();
+        Task<Store> GetStoreByIdAsync(string id, Currency currency = null);
     }
 }

VirtoCommerce.Storefront.Model/Stores/Store.cs
@@ -124,6 +124,8 @@ namespace VirtoCommerce.Storefront.Model.Stores
 
         public SeoLinksType SeoLinksType { get; set; }
 
+        public IList<PaymentMethod> PaymentMethods { get; set; }
+
         /// <summary>
         /// Checks if specified URL starts with store URL or store secure URL.
         /// </summary>

VirtoCommerce.Storefront/Controllers/Api/ApiOrderController.cs
@@ -4,7 +4,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using VirtoCommerce.Storefront.AutoRestClients.OrdersModuleApi;
-using VirtoCommerce.Storefront.AutoRestClients.StoreModuleApi;
 using VirtoCommerce.Storefront.Domain;
 using VirtoCommerce.Storefront.Domain.Security;
 using VirtoCommerce.Storefront.Infrastructure;
@@ -13,6 +12,7 @@ using VirtoCommerce.Storefront.Model;
 using VirtoCommerce.Storefront.Model.Common;
 using VirtoCommerce.Storefront.Model.Common.Exceptions;
 using VirtoCommerce.Storefront.Model.Order;
+using VirtoCommerce.Storefront.Model.Stores;
 using orderModel = VirtoCommerce.Storefront.AutoRestClients.OrdersModuleApi.Models;
 
 namespace VirtoCommerce.Storefront.Controllers.Api
@@ -21,14 +21,14 @@ namespace VirtoCommerce.Storefront.Controllers.Api
     public class ApiOrderController : StorefrontControllerBase
     {
         private readonly IOrderModule _orderApi;
-        private readonly IStoreModule _storeApi;
+        private readonly IStoreService _storeService;
         private readonly IAuthorizationService _authorizationService;
 
-        public ApiOrderController(IWorkContextAccessor workContextAccessor, IStorefrontUrlBuilder urlBuilder, IOrderModule orderApi, IStoreModule storeApi, IAuthorizationService authorizationService)
+        public ApiOrderController(IWorkContextAccessor workContextAccessor, IStorefrontUrlBuilder urlBuilder, IOrderModule orderApi, IStoreService storeService, IAuthorizationService authorizationService)
             : base(workContextAccessor, urlBuilder)
         {
             _orderApi = orderApi;
-            _storeApi = storeApi;
+            _storeService = storeService;
             _authorizationService = authorizationService;
         }
 
@@ -66,11 +66,7 @@ namespace VirtoCommerce.Storefront.Controllers.Api
         public async Task<ActionResult<NewPaymentData>> GetNewPaymentData(string orderNumber)
         {
             var order = await GetOrderByNumber(orderNumber);
-
-            var storeDto = await _storeApi.GetStoreByIdAsync(order.StoreId);
-            var paymentMethods = storeDto.PaymentMethods
-                                        .Where(x => x.IsActive.Value)
-                                        .Select(x => x.ToPaymentMethod(order));
+            var store = await _storeService.GetStoreByIdAsync(order.StoreId, order.Currency);
 
             var paymentDto = await _orderApi.GetNewPaymentAsync(order.Id);
             var payment = paymentDto.ToOrderInPayment(WorkContext.AllCurrencies, WorkContext.CurrentLanguage);
@@ -78,7 +74,7 @@ namespace VirtoCommerce.Storefront.Controllers.Api
             return new NewPaymentData
             {
                 Payment = payment,
-                PaymentMethods = paymentMethods,
+                PaymentMethods = store.PaymentMethods,
                 Order = order
             };
         }

VirtoCommerce.Storefront/DependencyInjection/ServiceCollectionExtension.cs
@@ -20,13 +20,16 @@ using VirtoCommerce.Storefront.AutoRestClients.CustomerModuleApi;
 using VirtoCommerce.Storefront.AutoRestClients.InventoryModuleApi;
 using VirtoCommerce.Storefront.AutoRestClients.MarketingModuleApi;
 using VirtoCommerce.Storefront.AutoRestClients.OrdersModuleApi;
+using VirtoCommerce.Storefront.AutoRestClients.PaymentModuleApi;
 using VirtoCommerce.Storefront.AutoRestClients.PlatformModuleApi;
 using VirtoCommerce.Storefront.AutoRestClients.PricingModuleApi;
 using VirtoCommerce.Storefront.AutoRestClients.ProductRecommendationsModuleApi;
 using VirtoCommerce.Storefront.AutoRestClients.QuoteModuleApi;
+using VirtoCommerce.Storefront.AutoRestClients.ShippingModuleApi;
 using VirtoCommerce.Storefront.AutoRestClients.SitemapsModuleApi;
 using VirtoCommerce.Storefront.AutoRestClients.StoreModuleApi;
 using VirtoCommerce.Storefront.AutoRestClients.SubscriptionModuleApi;
+using VirtoCommerce.Storefront.AutoRestClients.TaxModuleApi;
 using VirtoCommerce.Storefront.Domain;
 using VirtoCommerce.Storefront.Infrastructure;
 using VirtoCommerce.Storefront.Infrastructure.Autorest;
@@ -138,6 +141,12 @@ namespace VirtoCommerce.Storefront.DependencyInjection
             services.AddSingleton<ICacheModule>(sp => new CacheModule(sp.GetRequiredService<VirtoCommerceCacheRESTAPIdocumentation>()));
             services.AddAutoRestClient((credentials, httpClient, disposeHttpClient, baseUri) => new VirtoCommerceQuoteRESTAPIdocumentation(credentials, httpClient, disposeHttpClient) { BaseUri = baseUri });
             services.AddSingleton<IQuoteModule>(sp => new QuoteModule(sp.GetRequiredService<VirtoCommerceQuoteRESTAPIdocumentation>()));
+            services.AddAutoRestClient((credentials, httpClient, disposeHttpClient, baseUri) => new PaymentModule(credentials, httpClient, disposeHttpClient) { BaseUri = baseUri });
+            services.AddSingleton<IPaymentModule>(sp => sp.GetRequiredService<PaymentModule>());
+            services.AddAutoRestClient((credentials, httpClient, disposeHttpClient, baseUri) => new ShippingModule(credentials, httpClient, disposeHttpClient) { BaseUri = baseUri });
+            services.AddSingleton<IShippingModule>(sp => sp.GetRequiredService<ShippingModule>());
+            services.AddAutoRestClient((credentials, httpClient, disposeHttpClient, baseUri) => new TaxModule(credentials, httpClient, disposeHttpClient) { BaseUri = baseUri });
+            services.AddSingleton<ITaxModule>(sp => sp.GetRequiredService<TaxModule>());
 
             if (setupAction != null)
             {

commit 17ea76384f7b2f1acbf45e6ed4d00b6a1e2aebab

Fix breaking changes are introduced in #264 commit

VirtoCommerce.Storefront/Domain/Stores/StoreService.cs
@@ -17,7 +17,7 @@ using VirtoCommerce.Storefront.Model.Common.Caching;
 using VirtoCommerce.Storefront.Model.Stores;
 using StorePaymentMethod = VirtoCommerce.Storefront.AutoRestClients.StoreModuleApi.Models.PaymentMethod;
 using TaxProvider = VirtoCommerce.Storefront.AutoRestClients.StoreModuleApi.Models.TaxProvider;
-
+using StoreApi = VirtoCommerce.Storefront.AutoRestClients.StoreModuleApi.Models;
 
 namespace VirtoCommerce.Storefront.Domain
 {
@@ -50,13 +50,26 @@ namespace VirtoCommerce.Storefront.Domain
                 cacheEntry.AddExpirationToken(StoreCacheRegion.CreateChangeToken());
                 cacheEntry.AddExpirationToken(_apiChangesWatcher.CreateChangeToken());
 
-                return (await _storeApi.GetStoresAsync()).Select(x => x.ToStore()).ToArray();
+                var storeDtos = await _storeApi.GetStoresAsync();
+                return  await Task.WhenAll(storeDtos.Select(x => ConvertStoreAndLoadDependeciesAsync(x)));
             }, cacheNullValue: false);
         }
 
-        public async Task<Store> GetStoreByIdAsync(string id, Currency currency)
+        public async Task<Store> GetStoreByIdAsync(string id, Currency currency = null)
         {
-            var storeDto = await _storeApi.GetStoreByIdAsync(id);
+            var cacheKey = CacheKey.With(GetType(), "GetStoreByIdAsync");
+            return await _memoryCache.GetOrCreateExclusiveAsync(cacheKey, async (cacheEntry) =>
+            {
+                cacheEntry.AddExpirationToken(StoreCacheRegion.CreateChangeToken());
+                cacheEntry.AddExpirationToken(_apiChangesWatcher.CreateChangeToken());
+
+                var storeDto = await _storeApi.GetStoreByIdAsync(id);
+                return await ConvertStoreAndLoadDependeciesAsync(storeDto, currency);
+            }, cacheNullValue: false);
+        }
+
+        protected virtual async Task<Store> ConvertStoreAndLoadDependeciesAsync(StoreApi.Store storeDto, Currency currency = null)
+        {
             var result = storeDto.ToStore();
 
             if (!storeDto.PaymentMethods.IsNullOrEmpty() && currency != null)
@@ -67,7 +80,7 @@ namespace VirtoCommerce.Storefront.Domain
             }
             else if (storeDto.PaymentMethods.IsNullOrEmpty() && currency != null)
             {
-                var paymentSearchCriteria = new PaymentMethodsSearchCriteria { StoreId = id };
+                var paymentSearchCriteria = new PaymentMethodsSearchCriteria { StoreId = result.Id };
                 var paymentsSearchResult = await _paymentModule.SearchPaymentMethodsAsync(paymentSearchCriteria);
 
                 result.PaymentMethods = paymentsSearchResult.Results
@@ -82,7 +95,7 @@ namespace VirtoCommerce.Storefront.Domain
 
             if (storeDto.TaxProviders.IsNullOrEmpty())
             {
-                var taxSearchCriteria = new TaxProviderSearchCriteria { StoreId = id };
+                var taxSearchCriteria = new TaxProviderSearchCriteria { StoreId = result.Id };
                 var taxProviderSearchResult = await _taxModule.SearchTaxProvidersAsync(taxSearchCriteria);
                 result.FixedTaxRate = GetFixedTaxRate(taxProviderSearchResult.Results.Select(xp => xp.JsonConvert<TaxProvider>()).ToArray());
             }